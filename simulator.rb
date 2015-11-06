08436c1a66c83bb7d1b49a1baf5ef88cc1d556ba
chiprockstar <chiprockstar@users.noreply.github.com>
2015-11-06 16:40:26 -0500

    Update simulator.rb

--- simulator.rb
+++ simulator.rb
@@ -1,26 +1,22 @@
 class Robot
 
-  def initialize(direction=:east, x=0, y=0)
-    @direction = direction
-    @x = x
-    @y = y
-  end
+attr_reader :bearing
+
 
   def orient(direction)
     raise ArgumentError.new if [:north, :south, :east, :west].include?(direction) == false
-    bearing
-    @direction = direction
+    @bearing = direction
   end
 
-  def bearing
-    @direction
-  end
+  # def bearing
+  #   @bearing #-> attr_reader replaces this for bearing getter
+  # end
 
   def turn(dir)
-    return dir == 'R' ? @direction = :east  : @direction = :west  if @direction == :north
-    return dir == 'R' ? @direction = :south : @direction = :north if @direction == :east
-    return dir == 'R' ? @direction = :north : @direction = :south if @direction == :west
-    return dir == 'R' ? @direction = :west  : @direction = :east  if @direction == :south
+    return dir == 'R' ? @bearing = :east  : @bearing = :west  if @bearing == :north
+    return dir == 'R' ? @bearing = :south : @bearing = :north if @bearing == :east
+    return dir == 'R' ? @bearing = :north : @bearing = :south if @bearing == :west
+    return dir == 'R' ? @bearing = :west  : @bearing = :east  if @bearing == :south
   end
 
   def turn_left
@@ -34,7 +30,6 @@ class Robot
   def at(x, y)
     @x = x
     @y = y
-    coordinates
   end
 
   def coordinates
@@ -42,16 +37,10 @@ class Robot
   end
 
   def advance
-    @y += 1 if @direction == :north
-    @x += 1 if @direction == :east
-    @y -= 1 if @direction == :south
-    @x -= 1 if @direction == :west
-  end
-
-  def place_robot(x, y, direction)
-    @x = x
-    @y = y
-    @direction = direction
+    @y += 1 if @bearing == :north
+    @x += 1 if @bearing == :east
+    @y -= 1 if @bearing == :south
+    @x -= 1 if @bearing == :west
   end
 
 end
@@ -61,26 +50,25 @@ class Simulator
   attr_accessor :x, :y, :direction
 
   def instructions(commands)
-    output = []
-    cmd_arr = commands.split("")
-    cmd_arr.each do |command|
-      output << [:turn_left]  if command == 'L'
-      output << [:turn_right] if command == 'R'
-      output << [:advance]    if command == 'A'
-    end
-    output.flatten!
+      commands.chars.map do |command|
+        if command == 'L'
+          :turn_left
+        elsif command == 'R'
+          :turn_right
+        elsif command == 'A'
+          :advance
+        end
+      end
   end
 
-  def place(robot, x:, y:, direction:)
-    robot.place_robot(x , y, direction)
+  def place(robot, placement)
+    robot.at(placement[:x], placement[:y])
+      robot.orient(placement[:direction])
   end
 
   def evaluate(robot, commands)
-    cmd_arr = instructions(commands)
-    cmd_arr.each do |command|
-      robot.turn_right if command == :turn_right
-      robot.turn_left  if command == :turn_left
-      robot.advance    if command == :advance
+      instructions(commands).each do |command|
+      robot.send(command)
     end
   end
 end
